4deabef4895b04584ea52d11ac3d8dd902baa398
Tania Ponomarenko <tane4ka377@gmail.com>
2023-07-12 20:41:20 +0300

    change transliteration rules for yu, ya, ye, yi, y

--- src/test.py
+++ src/test.py
@@ -1,36 +1,71 @@
 import unittest
-from ua_latin import word_translate
+from ua_latin import text_translate
 
 
 class TestWordTranslate(unittest.TestCase):
-    def test_lowercase_one_word(self):
+    def test_all_letters(self):
         source_text = ("Жебракують філософи при ґанку церкви в Гадячі, "
                        "ще й шатро їхнє п'яне знаємо")
         expected_translation = ("Zhebrakuiut filosofy pry ganku tserkvy v "
-                                "Hadiachi, shche i shatro ikhnie piane "
+                                "Hadiachi, shche y shatro yikhnie piane "
                                 "znaiemo")
-        actual_translation = word_translate(source_text)
+        actual_translation = text_translate(source_text)
 
         self.assertEqual(actual_translation, expected_translation)
 
     def test_uppercase_one_word(self):
         source_text = "ПаляНиця"
         expected_translation = "PaliaNytsia"
-        actual_translation = word_translate(source_text)
+        actual_translation = text_translate(source_text)
 
         self.assertEqual(actual_translation, expected_translation)
 
     def test_several_words(self):
         source_text = "Слава Україні!"
         expected_translation = "Slava Ukraini!"
-        actual_translation = word_translate(source_text)
+        actual_translation = text_translate(source_text)
 
         self.assertEqual(actual_translation, expected_translation)
 
     def test_word_with_latin_symbol(self):
         source_text = "Я програмую на Python ^_^"
         expected_translation = "Ya prohramuiu na Python ^_^"
-        actual_translation = word_translate(source_text)
+        actual_translation = text_translate(source_text)
+
+        self.assertEqual(actual_translation, expected_translation)
+
+    def test_word_with_Yu_symbol(self):
+        source_text = "юю"
+        expected_translation = "yuiu"
+        actual_translation = text_translate(source_text)
+
+        self.assertEqual(actual_translation, expected_translation)
+
+    def test_word_with_Ya_symbol(self):
+        source_text = "яя"
+        expected_translation = "yaia"
+        actual_translation = text_translate(source_text)
+
+        self.assertEqual(actual_translation, expected_translation)
+
+    def test_word_with_Y_symbol(self):
+        source_text = "йой"
+        expected_translation = "yoi"
+        actual_translation = text_translate(source_text)
+
+        self.assertEqual(actual_translation, expected_translation)
+
+    def test_word_with_Yi_symbol(self):
+        source_text = "її"
+        expected_translation = "yii"
+        actual_translation = text_translate(source_text)
+
+        self.assertEqual(actual_translation, expected_translation)
+
+    def test_word_with_Ye_symbol(self):
+        source_text = "єє"
+        expected_translation = "yeie"
+        actual_translation = text_translate(source_text)
 
         self.assertEqual(actual_translation, expected_translation)
 
