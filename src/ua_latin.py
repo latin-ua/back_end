e8f08afe241c899be9687ca3910b30167383fa54
Anna Strukova <metiletan@gmail.com>
2023-07-25 18:32:21 +0200

    fix code: not to decode to utf-8 and orginise imports

--- src/ua_latin.py
+++ src/ua_latin.py
@@ -1,7 +1,8 @@
+import logging
+
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
-import logging
 from ua_latin_kmu import text_translate_kmu
 from ua_latin_dstu_a import text_translate_dstu_a
 
@@ -12,30 +13,20 @@ service = Flask("latin-ua-service")
 CORS(service)
 
 
-# @service.post("/")
-# @cross_origin()
-# def translate_text_endpoint():
-#     source_text = request.data.decode("utf8")
-#     logger.error(f"Received request: {source_text}")
-#     return text_translate_system_a(source_text), 200
-
-
-@service.post("/json")
+@service.post("/")
 @cross_origin()
 def translate_text_endpoint():
-    sourse_data = request.json
-    translationMethod = sourse_data.get("translationMethod")
-    received_text = sourse_data.get("text")
-    source_text = received_text.decode("utf8")
+    source_data = request.json
+    translation_method = source_data.get("translationMethod")
+    source_text = source_data.get("text")
 
-    if translationMethod == "KMU":
-        logger.error(f"Received request: {source_text}")
+    logger.info(f"Received request: {source_text}")
+
+    if translation_method == "KMU":
         return text_translate_kmu(source_text), 200
-    elif translationMethod == "DSTU_A":
-        logger.error(f"Received request: {source_text}")
+    elif translation_method == "DSTU_A":
         return text_translate_dstu_a(source_text), 200
     else:
-        logger.error(f"Received request: {source_text}")
         return "Translation method must be one of: 'KMU', 'DSTU_A'", 400
 
 
